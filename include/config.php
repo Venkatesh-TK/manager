579e6f7da5977d4eaddef915e225d7250cba60b4
Venkatesh-TK <114131840+Venkatesh-TK@users.noreply.github.com>
2022-11-10 19:23:46 +0530

    first commit

---
+++ include/config.php
@@ -0,0 +1,14 @@
+<?php
+class dbConfig {
+    protected $serverName;
+    protected $userName;
+    protected $password;
+    protected $dbName;
+    function dbConfig() {
+        $this -> serverName = '127.0.0.1:4306';
+        $this -> userName = 'root';
+        $this -> password = '';
+        $this -> dbName = 'test2';
+    }
+}
+?>
